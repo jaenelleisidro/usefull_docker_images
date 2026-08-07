e3082de75ecfe249d63ed39c83c91027224154fd
jaenelle isidro <jaenelleisidro@gmail.com>
2026-08-08 04:37:12 +0800

    added a simple java crud rest api and dockerized it

---
+++ developer/java/crud/src/main/java/com/template/crud/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.template.crud.repository;
+import com.template.crud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
