b270c941001e1424c1a79a17306ff1b8c0468fc2
jaenelleisidro <jaenelleisidro@yahoo.com>
2026-08-15 05:29:45 +0800

    added some example audio for transcribing. made portainer .env relative

--- developer/nodejs/nodejs_ai_transcript/index.js
+++ developer/nodejs/nodejs_ai_transcript/index.js
@@ -7,20 +7,31 @@ const __dirname = dirname(__filename);
 
 
 
-const filePath = path.resolve(__dirname, 'sound.mp3')
+// const filePath = path.resolve(__dirname,'audio','japan.m4a')
+const filePath = path.resolve(__dirname,'audio','english.m4a')
 const modelRootPath = path.resolve(__dirname, 'data','whisper-models')
 
-// const filePath = './sound.mp3';
-// const modelRootPath = './data/whisper-models';
+const models={
+  tiny: "tiny",
+  tiny_en: "tiny.en",
+  base: "base",
+  base_en: "base.en",
+  small: "small",
+  small_en: "small.en",
+  medium: "medium",
+  medium_en: "medium.en",
+  large_v1: "large-v1",
+  large: "large",
+  large_v3_turbo: "large-v3-turbo"
+}
 
-const modelName='tiny.en';
-
+const modelName=models.large_v3_turbo;
 await nodewhisper(filePath, {
     modelName,
     autoDownloadModelName: modelName,
     modelRootPath,
     whisperOptions: {
         outputInSrt: true,
-        noGpu:true
+        // noGpu:true
     },
 })
---
+++ developer/nodejs/nodejs_ai_transcript/setup.sh
@@ -0,0 +1,10 @@
+# sudo apt update
+# sudo apt install nvidia-cuda-toolkit
+
+
+
+sudo apt update
+sudo apt install cmake
+
+sudo apt install build-essential
+npx nodejs-whisper download
