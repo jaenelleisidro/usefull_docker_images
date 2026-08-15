8172a606e1011c9555388dde64b8942f5aae4171
jaenelleisidro <jaenelleisidro@yahoo.com>
2026-08-15 15:27:35 +0800

    code cleaning

--- developer/nodejs/nodejs_langchain/setup.sh
+++ developer/nodejs/nodejs_langchain/setup.sh
@@ -1,2 +1,12 @@
-docker exec -it ollama ollama run nomic-embed-text
-docker exec -it ollama ollama run qwen3.5:4b
+docker exec -it ollama-app ollama run nomic-embed-text
+docker exec -it ollama-app ollama run qwen3.5:4b
+docker exec -it ollama-app ollama run qwen2.5-coder:0.5b
+docker exec -it ollama-app ollama run qwen2.5-coder:1.5b
+docker exec -it ollama-app ollama run qwen2.5-coder:3b
+docker exec -it ollama-app ollama run qwen2.5-coder:7b
+docker exec -it ollama-app ollama run ALIENTELLIGENCE/doctorai
+docker exec -it ollama-app ollama run medgemma:4b
+docker exec -it ollama-app ollama run medgemma:27b
+docker exec -it ollama-app ollama run thewindmom/llama3-med42-8b:latest
+
+
